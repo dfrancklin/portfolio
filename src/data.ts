a9af950ebe7ab0ff131780f4b16948a883584082
Diego Francklin <dfrancklin23@gmail.com>
2023-12-02 16:18:22 -0300

    refactor: :truck: moved data to separate file and moved header and footer to layout

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+export const startYear = 2011;
+export const currentYear = new Date().getFullYear();
+export const whatsAppLink = "https://wa.me/5515991456918";
+export const cvLink = "/Diego Francklin Martins dos Santos | CV | PT.pdf";
+export const email = "dfrancklin23@gmail.com";
+
+export const navigation = [
+  { name: "Home", href: "/" },
+  { name: "Experiência", badge: "Em breve" },
+  { name: "Projetos", badge: "Em breve" },
+];
+
+export const social = [
+  {
+    name: "GitHub",
+    icon: "mdi:github",
+    href: "https://github.com/dfrancklin",
+    target: "_blank",
+  },
+  {
+    name: "LinkedIn",
+    icon: "mdi:linkedin",
+    href: "https://www.linkedin.com/in/diego-francklin-martins-dos-santos-599032a6/",
+    target: "_blank",
+  },
+  {
+    name: "Email",
+    icon: "mdi:email-outline",
+    href: `mailto:${email}`,
+    target: "_blank",
+  },
+];
