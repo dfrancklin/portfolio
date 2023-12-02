331fa67d3693a5ad65b3742c43cc1b101151e39c
Diego Francklin <dfrancklin23@gmail.com>
2023-12-02 16:25:08 -0300

    feat: :dizzy: added spin animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "counter-spin": {
+          "100%": { transform: "rotate(-360deg)" },
+        },
+      },
+      animation: {
+        "spin-slow": "spin 120s linear infinite",
+        "counter-spin-slow": "counter-spin 120s linear infinite",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
